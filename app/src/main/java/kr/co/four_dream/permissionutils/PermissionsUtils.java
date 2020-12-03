a695c06aee6aaea0fa8a76017a2d3e9dc9f1b1d8
NewLifeOldSong <295516829@qq.com>
2020-12-03 12:58:00 +0900

    Update PermissionsUtils.java

--- app/src/main/java/kr/co/four_dream/permissionutils/PermissionsUtils.java
+++ app/src/main/java/kr/co/four_dream/permissionutils/PermissionsUtils.java
@@ -11,79 +11,97 @@ import androidx.core.content.ContextCompat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PermissionsUtils {
+public class PermissionUtil {
 
-    private PermissionsUtils() {
+    private PermissionUtil() {
     }
 
-    private static PermissionsUtils permissionsUtils;
-    private IPermissionsResult mPermissionsResult;
+    private static PermissionUtil permissionUtil;
+    private IPermissionsResult iPermissionsResult;
 
-    public static PermissionsUtils getInstance() {
-        if (permissionsUtils == null) {
-            permissionsUtils = new PermissionsUtils();
+    private List<String> needPermissionList;
+
+    public static PermissionUtil getInstance() {
+        if (permissionUtil == null) {
+            permissionUtil = new PermissionUtil();
         }
-        return permissionsUtils;
+        return permissionUtil;
     }
+
+    /**
+     * 선택 권한 예제.
+     */
+    private final String UNNECESSARY_FOR_ACCESS_BACKGROUND_LOCATION = "ACCESS_BACKGROUND_LOCATION";
 
     public void checkPermissions(Activity activity, String[] permissions, @NonNull IPermissionsResult permissionsResult, int REQUEST_CODE) {
-        mPermissionsResult = permissionsResult;
+        iPermissionsResult = permissionsResult;
 
-        //6.0 以上才需要动态权限
         if (Build.VERSION.SDK_INT < 23) {
             permissionsResult.permissionPass();
             return;
         }
 
-        //创建一个mPermissionList，逐个判断哪些权限未授予，未授予的权限存储到needPermissionList中
-        List<String> needPermissionList = new ArrayList<>();
-        //逐个判断你要的权限是否已经通过
+        needPermissionList = new ArrayList<>();
+
         for (int i = 0; i < permissions.length; i++) {
             if (ContextCompat.checkSelfPermission(activity, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
-                needPermissionList.add(permissions[i]);//添加还未授予的权限/통과되지 못한 권함을 추가
+                // 선택 권한
+                if (permissions[i].contains(UNNECESSARY_FOR_ACCESS_BACKGROUND_LOCATION)) {
+
+                } else {
+                    // 필수 권한을 리스트에 추가
+                    needPermissionList.add(permissions[i]);
+                }
             }
         }
 
-        //申请权限
+        // start request
         if (needPermissionList.size() > 0) {
-            //有权限没有通过，需要申请
+
             ActivityCompat.requestPermissions(activity, permissions, REQUEST_CODE);
+
         } else {
-            //说明权限都已经通过，可以做你想做的事情去
+            // permission all pass.
             permissionsResult.permissionPass();
 
         }
-
-
     }
 
     public void onRequestPermissionsResult(final Activity context, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         boolean hasAllGranted = true;
+
         for (int i = 0; i < grantResults.length; i++) {
             if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+
+                // 선택 권한
+                if (permissions[i].contains(UNNECESSARY_FOR_ACCESS_BACKGROUND_LOCATION)) {
+                    break;
+                }
+
                 hasAllGranted = false;
                 //在用户已经拒绝授权的情况下，如果shouldShowRequestPermissionRationale返回false则
                 // 可以推断出用户选择了“不在提示”选项，在这种情况下需要引导用户至设置页手动授权
                 if (!ActivityCompat.shouldShowRequestPermissionRationale(context, permissions[i])) {
 
-                    mPermissionsResult.permissionNoPassNaver();
+                    iPermissionsResult.permissionNoPassNever();
 
                 } else {
                     //权限请求失败，但未选中“不再提示”选项
 
-                    mPermissionsResult.permissionNoPass();
+                    iPermissionsResult.permissionNoPass();
                 }
+
+//                }
                 break;
             }
         }
         if (hasAllGranted) {
-            //全部权限通过，可以进行下一步操作。。。
-            mPermissionsResult.permissionPass();
+            // permission all pass.
+            iPermissionsResult.permissionPass();
         }
 
     }
-
 
     public interface IPermissionsResult {
         /**
@@ -99,6 +117,6 @@ public class PermissionsUtils {
         /**
          * 权限未通过且点击了不再显示
          */
-        void permissionNoPassNaver();
+        void permissionNoPassNever();
     }
 }
